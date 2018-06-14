7170654d03b8776302989027b62411865597de32
Edward Rowe <edward.l.rowe@gmail.com>
2018-06-13 20:48:05 -0400

    Put StateTests in a testing namespace

--- Assets/RedBlueGames/LiteFSM/Tests/Editor/StateTests.cs
+++ Assets/RedBlueGames/LiteFSM/Tests/Editor/StateTests.cs
@@ -1,101 +1,103 @@
-﻿using NUnit.Framework;
-using RedBlueGames.LiteFSM;
-
-public class StateTests
+﻿namespace RedBlueGames.LiteFSM.Tests
 {
-    private enum StateID
+    using NUnit.Framework;
+
+    public class StateTests
     {
-        SingleState,
-    }
+        private enum StateID
+        {
+            SingleState,
+        }
 
-    [Test]
-    public void Enter_ValidState_InvokesCallback()
-    {
-        // Arrange
-        var enterCalled = false;
-        State<StateID>.EnterMethod enterMethod = () => enterCalled = true;
-        var state = new State<StateID>(StateID.SingleState, enterMethod, null, null);
+        [Test]
+        public void Enter_ValidState_InvokesCallback()
+        {
+            // Arrange
+            var enterCalled = false;
+            State<StateID>.EnterMethod enterMethod = () => enterCalled = true;
+            var state = new State<StateID>(StateID.SingleState, enterMethod, null, null);
 
-        // Act
-        state.Enter();
+            // Act
+            state.Enter();
 
-        // Assert
-        Assert.IsTrue(enterCalled);
-    }
+            // Assert
+            Assert.IsTrue(enterCalled);
+        }
 
-    [Test]
-    public void Enter_NullEnterMethod_DoesNotThrow()
-    {
-        // Arrange
-        var state = new State<StateID>(StateID.SingleState, null, null, null);
+        [Test]
+        public void Enter_NullEnterMethod_DoesNotThrow()
+        {
+            // Arrange
+            var state = new State<StateID>(StateID.SingleState, null, null, null);
 
-        // Assert
-        Assert.DoesNotThrow(() => state.Enter());
-    }
+            // Assert
+            Assert.DoesNotThrow(() => state.Enter());
+        }
 
-    [Test]
-    public void Exit_ValidState_InvokesCallback()
-    {
-        // Arrange
-        var exitCalled = false;
-        State<StateID>.ExitMethod exitMethod = () => exitCalled = true;
-        var state = new State<StateID>(StateID.SingleState, null, exitMethod, null);
+        [Test]
+        public void Exit_ValidState_InvokesCallback()
+        {
+            // Arrange
+            var exitCalled = false;
+            State<StateID>.ExitMethod exitMethod = () => exitCalled = true;
+            var state = new State<StateID>(StateID.SingleState, null, exitMethod, null);
 
-        // Act
-        state.Exit();
+            // Act
+            state.Exit();
 
-        // Assert
-        Assert.IsTrue(exitCalled);
-    }
+            // Assert
+            Assert.IsTrue(exitCalled);
+        }
 
-    [Test]
-    public void Exit_NullExitMethod_DoesNotThrow()
-    {
-        // Arrange
-        var state = new State<StateID>(StateID.SingleState, null, null, null);
+        [Test]
+        public void Exit_NullExitMethod_DoesNotThrow()
+        {
+            // Arrange
+            var state = new State<StateID>(StateID.SingleState, null, null, null);
 
-        // Assert
-        Assert.DoesNotThrow(() => state.Exit());
-    }
+            // Assert
+            Assert.DoesNotThrow(() => state.Exit());
+        }
 
-    [Test]
-    public void Update_ValidState_InvokesCallback()
-    {
-        // Arrange
-        var updateCalled = false;
-        State<StateID>.UpdateMethod updateMethod = (dT) => updateCalled = true;
-        var state = new State<StateID>(StateID.SingleState, null, null, updateMethod);
+        [Test]
+        public void Update_ValidState_InvokesCallback()
+        {
+            // Arrange
+            var updateCalled = false;
+            State<StateID>.UpdateMethod updateMethod = (dT) => updateCalled = true;
+            var state = new State<StateID>(StateID.SingleState, null, null, updateMethod);
 
-        // Act
-        state.Update(0.0f);
+            // Act
+            state.Update(0.0f);
 
-        // Assert
-        Assert.IsTrue(updateCalled);
-    }
+            // Assert
+            Assert.IsTrue(updateCalled);
+        }
 
-    [Test]
-    public void Update_ValidDeltaTime_PaasesValidTime()
-    {
-        // Arrange
-        var expectedDeltaTime = 1.1f;
-        var reportedTime = 0.0f;
-        State<StateID>.UpdateMethod updateMethod = (dT) => reportedTime = dT;
-        var state = new State<StateID>(StateID.SingleState, null, null, updateMethod);
+        [Test]
+        public void Update_ValidDeltaTime_PaasesValidTime()
+        {
+            // Arrange
+            var expectedDeltaTime = 1.1f;
+            var reportedTime = 0.0f;
+            State<StateID>.UpdateMethod updateMethod = (dT) => reportedTime = dT;
+            var state = new State<StateID>(StateID.SingleState, null, null, updateMethod);
 
-        // Act
-        state.Update(1.1f);
+            // Act
+            state.Update(1.1f);
 
-        // Assert
-        Assert.AreEqual(expectedDeltaTime, reportedTime);
-    }
+            // Assert
+            Assert.AreEqual(expectedDeltaTime, reportedTime);
+        }
 
-    [Test]
-    public void Update_NullUpdateMethod_DoesNotThrow()
-    {
-        // Arrange
-        var state = new State<StateID>(StateID.SingleState, null, null, null);
+        [Test]
+        public void Update_NullUpdateMethod_DoesNotThrow()
+        {
+            // Arrange
+            var state = new State<StateID>(StateID.SingleState, null, null, null);
 
-        // Assert
-        Assert.DoesNotThrow(() => state.Update(0.0f));
+            // Assert
+            Assert.DoesNotThrow(() => state.Update(0.0f));
+        }
     }
 }
