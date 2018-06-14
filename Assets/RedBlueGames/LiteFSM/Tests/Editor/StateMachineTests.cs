12db6555a603fb753ec4cccf56cc27d6662977b1
Edward Rowe <edward.l.rowe@gmail.com>
2018-06-13 21:09:43 -0400

    Add constructor test for StateMachine, including a state stub

---
+++ Assets/RedBlueGames/LiteFSM/Tests/Editor/StateMachineTests.cs
@@ -0,0 +1,27 @@
+﻿namespace RedBlueGames.LiteFSM.Tests
+{
+    using UnityEngine;
+    using UnityEngine.TestTools;
+    using NUnit.Framework;
+    using System.Collections;
+    using RedBlueGames.LiteFSM;
+
+    public class StateMachineTests
+    {
+        [Test]
+        public void Ctor_NoReflection_EntersInitialState()
+        {
+            // Arrange
+            var stubStates = new StubState<SingleStateID>[]
+            {
+                new StubState<SingleStateID>(SingleStateID.Init)
+            };
+
+            // Act
+            new StateMachine<SingleStateID>(stubStates, SingleStateID.Init);
+
+            // Assert
+            Assert.IsTrue(stubStates[0].EnterCalled);
+        }
+    }
+}
---
+++ Assets/RedBlueGames/LiteFSM/Tests/Editor/StateStub.cs
@@ -0,0 +1,40 @@
+﻿namespace RedBlueGames.LiteFSM.Tests
+{
+    using System;
+
+    public enum SingleStateID
+    {
+        Init
+    }
+
+    public class StubState<T> : IState<T> where T : struct, IConvertible, IComparable, IFormattable
+    {
+        public bool EnterCalled { get; private set; }
+        public bool ExitCalled { get; private set; }
+        public bool UpdateCalled { get; private set; }
+        public float LastDeltaTime { get; private set; }
+
+        public T ID { get; private set; }
+
+        public StubState(T id)
+        {
+            this.ID = id;
+        }
+
+        public void Enter()
+        {
+            this.EnterCalled = true;
+        }
+
+        public void Exit()
+        {
+            this.ExitCalled = true;
+        }
+
+        public void Update(float dT)
+        {
+            this.UpdateCalled = true;
+            this.LastDeltaTime = dT;
+        }
+    }
+}
