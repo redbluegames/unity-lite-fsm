d7780995a4255cee68255d6f79b0c9eb51999237
Edward Rowe <edward.l.rowe@gmail.com>
2018-06-21 01:48:02 -0400

    Verify states passed to StateMachines match entries in T

--- Assets/RedBlueGames/LiteFSM/StateMachine.cs
+++ Assets/RedBlueGames/LiteFSM/StateMachine.cs
@@ -49,24 +49,9 @@
 
         private void Initialize(IState<T>[] states, T initialStateID)
         {
-            // Validate that all entries of T are supplied
-            var missingEntries = GetMissingIDs(states);
-            if (missingEntries.Length > 0)
-            {
-                var message = string.Concat(
-                    "StateMachine trying to initialize with an invalid set of states. " +
-                    "Not enough states passed in. Missing states:");
-                foreach (var missingEntry in missingEntries)
-                {
-                    message = string.Concat(message, missingEntry.ToString(), ", ");
-                }
-
-                throw new System.ArgumentException(message);
-            }
+            this.VerifyAllStatesExistInT(states);
 
             /*
-            // TODO: Too many entries (doubled up)
-
             // TODO: Entries of Different types of T? Like, two different enums?
 
             // TODO: Test it's an enum (typeof(T).IsEnum)
@@ -81,6 +66,37 @@
             // TODO: Validate desiredState is in the dictionary
             this.currentStateID = initialStateID;
             this.CurrentState.Enter();
+        }
+
+        private void VerifyAllStatesExistInT(IState<T>[] states)
+        {
+            var entriesInT = Enum.GetValues(typeof(T));
+            if (states.Length != entriesInT.Length)
+            {
+                this.VerifyStatesArentMissing(states);
+                this.VerifyNoStatesAreDuplicates(states);
+            }
+        }
+
+        private void VerifyStatesArentMissing(IState<T>[] states)
+        {
+            var missingEntries = GetMissingIDs(states);
+            if (missingEntries.Length > 0)
+            {
+                var message = string.Concat(
+                    "StateMachine trying to initialize with an invalid set of states. " +
+                    "Not enough states passed in. Missing states: ");
+                foreach (var missingEntry in missingEntries)
+                {
+                    message = string.Concat(message, missingEntry.ToString());
+                    if (!missingEntry.Equals(missingEntries[missingEntries.Length - 1]))
+                    {
+                        message = string.Concat(message, ", ");
+                    }
+                }
+
+                throw new System.ArgumentException(message);
+            }
         }
 
         private static T[] GetMissingIDs(IState<T>[] states)
@@ -103,6 +119,48 @@
             }
 
             return missingTs.ToArray();
+        }
+
+        private void VerifyNoStatesAreDuplicates(IState<T>[] states)
+        {
+            var duplicateIDs = GetDuplicateIDs(states);
+            if (duplicateIDs.Length > 0)
+            {
+                var message = string.Concat(
+                    "StateMachine trying to initialize with an invalid set of states. " +
+                    "Duplicate states passed in. Duplicate states: ");
+                foreach (var duplicateEntry in duplicateIDs)
+                {
+                    message = string.Concat(message, duplicateEntry.ToString());
+                    if (!duplicateEntry.Equals(duplicateIDs[duplicateIDs.Length - 1]))
+                    {
+                        message = string.Concat(message, ", ");
+                    }
+                }
+
+                throw new System.ArgumentException(message);
+            }
+        }
+
+        private static T[] GetDuplicateIDs(IState<T>[] states)
+        {
+            var extraStates = new List<T>();
+            foreach (var state in states)
+            {
+                extraStates.Add(state.ID);
+            }
+
+            var entriesInT = Enum.GetValues(typeof(T));
+            for (int i = 0; i < entriesInT.Length; i++)
+            {
+                var entry = (T)entriesInT.GetValue(i);
+                if (extraStates.Contains(entry))
+                {
+                    extraStates.Remove(entry);
+                }
+            }
+
+            return extraStates.ToArray();
         }
     }
 }
--- Assets/RedBlueGames/LiteFSM/Tests/Editor/StateMachineTests.cs
+++ Assets/RedBlueGames/LiteFSM/Tests/Editor/StateMachineTests.cs
@@ -19,8 +19,8 @@
             new StateMachine<TwoStatesID>(stubStates, TwoStatesID.One);
 
             // Assert
-            Assert.IsTrue(stubStates[0].EnterCalled);
-            Assert.IsFalse(stubStates[1].EnterCalled);
+            Assert.That(stubStates[0].EnterCalled, Is.True);
+            Assert.That(stubStates[1].EnterCalled, Is.False);
         }
 
         [Test]
@@ -32,9 +32,35 @@
                 new StubState<TwoStatesID>(TwoStatesID.One)
             };
 
+            var expectedMessage = string.Concat(
+                    "StateMachine trying to initialize with an invalid set of states. " +
+                    "Not enough states passed in. Missing states: Two");
+
             // Act / Assert
-            Assert.Throws(typeof(System.ArgumentException),
-                          () => new StateMachine<TwoStatesID>(stubStates, TwoStatesID.One));
+            var exception = Assert.Throws<System.ArgumentException>(
+                () => new StateMachine<TwoStatesID>(stubStates, TwoStatesID.One));
+            Assert.That(exception.Message, Is.EqualTo(expectedMessage));
+        }
+
+        [Test]
+        public void CtorNoReflection_DuplicateStates_Throws()
+        {
+            // Arrange
+            var stubStates = new StubState<TwoStatesID>[]
+            {
+                new StubState<TwoStatesID>(TwoStatesID.One),
+                new StubState<TwoStatesID>(TwoStatesID.One),
+                new StubState<TwoStatesID>(TwoStatesID.Two),
+            };
+
+            var expectedMessage = string.Concat(
+                    "StateMachine trying to initialize with an invalid set of states. " +
+                    "Duplicate states passed in. Duplicate states: One");
+
+            // Act / Assert
+            var exception = Assert.Throws<System.ArgumentException>(
+                () => new StateMachine<TwoStatesID>(stubStates, TwoStatesID.One));
+            Assert.That(exception.Message, Is.EqualTo(expectedMessage));
         }
     }
 }
